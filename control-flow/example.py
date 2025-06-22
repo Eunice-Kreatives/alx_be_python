94357b582863d55f5be60d2bc52c99974217e6d2
Eunice Akpa <akpaeunice2242@gmail.com>
2025-06-22 23:36:17 +0100

    Update Daily reminder file

--- control-flow/example.py
+++ control-flow/example.py
@@ -1,7 +1,10 @@
-number = int(input("Enter a number to see its multiplication table: "))
-for i in range (1, 11):
-    product = number * i
-    print(f"{number} *{i} = {product}")
-    
+size = int(input("Enter the size of the pattern: "))
+row = 0
+while row < size:
+    for i in range(size):
+        
+        print("*" ,end= " ")
+    print()
+    row +=1
 
         
