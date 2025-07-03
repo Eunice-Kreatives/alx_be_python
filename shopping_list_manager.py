b39e5bb6dbe4c27a754249c1572f900cef79de13
Eunice Akpa <akpaeunice2242@gmail.com>
2025-07-03 21:15:30 +0100

    Update my shopping list file

--- shopping_list_manager.py
+++ shopping_list_manager.py
@@ -2,7 +2,7 @@
 
 def display_menu():
 
-    print("\n--- Shopping List Manager ---\n")
+    print("--- Shopping List Manager ---")
     print("1. Add item")
     print("2. Remove item")
     print("3. View list")
